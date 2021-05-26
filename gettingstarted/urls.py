d289103918de3f212b8c4f9991a2cfc2af930967
ninalutz <nlutz@mit.edu>
2021-05-26 16:03:03 -0400

    add years

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -9,9 +9,9 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("admin/", admin.site.urls),
-    path('transcriber_stats/<start_month>/<start_day>/<end_month>/<end_day>', hello.views.transcriber_stats, name="transcriber_stats"),
-    path('languages_stats/<city>/<start_month>/<start_day>/<end_month>/<end_day>', hello.views.languages_stats, name="langauges_stats"),
-    path('new_languages/<start_month>/<start_day>/<end_month>/<end_day>', hello.views.new_languages_stats, name="new_stats"),
-    path('stats/<city>/<start_month>/<start_day>/<end_month>/<end_day>', hello.views.stats, name="stats"),
+    path('transcriber_stats/<start_month>/<start_day>/<start_year>/<end_month>/<end_day>/<end_year>', hello.views.transcriber_stats, name="transcriber_stats"),
+    path('languages_stats/<city>/<start_month>/<start_day>/<start_year>/<end_month>/<end_day>/<end_year>', hello.views.languages_stats, name="langauges_stats"),
+    path('new_languages/<start_month>/<start_day>/<start_year>/<end_month>/<end_day>/<end_year>', hello.views.new_languages_stats, name="new_stats"),
+    path('stats/<city>/<start_month>/<start_day>/<start_year>/<end_month>/<end_day>/<end_year>', hello.views.stats, name="stats"),
     path('untranscribed_languages/', hello.views.untranscribed_languages, name="untranscribed")
 ]
--- hello/views.py
+++ hello/views.py
@@ -34,7 +34,7 @@ TODO: Add a year parameter //added 2/20/21
 Test links: *will change*
 """
 @csrf_exempt
-def stats(request, city, start_year, start_month, start_day, end_year, end_month, end_day):
+def stats(request, city, start_month, start_day, start_year, end_month, end_day, end_year):
     out = {}
     start = datetime(int(start_year), int(start_month), int(start_day), 0, 0, 0)
     end = datetime(int(end_year), int(end_month), int(end_day), 0, 0, 0)
@@ -48,7 +48,7 @@ Gets language stats over time from a city -- New transcripts by month sheet
 TODO: Add a year parameter //added 2/20/21
 """
 @csrf_exempt
-def languages_stats(request, city, start_year, start_month, start_day, end_year, end_month, end_day):
+def languages_stats(request, city, start_month, start_day, start_year, end_month, end_day, end_year):
     start = datetime(int(start_year), int(start_month), int(start_day), 0, 0, 0)
     end = datetime(int(end_year), int(end_month), int(end_day), 0, 0, 0)
     out = get_languages_over_time(city, start, end)
@@ -60,7 +60,7 @@ Tells us languages that were added in a date range -- New languages sheet
 TODDO: Add a year parameter //added 2/20/21
 """
 @csrf_exempt
-def new_languages_stats(request, start_year, start_month, start_day, end_year, end_month, end_day):
+def new_languages_stats(request, start_month, start_day, start_year, end_month, end_day, end_year):
     start = datetime(int(start_year), int(start_month), int(start_day), 0, 0, 0)
     end = datetime(int(end_year), int(end_month), int(end_day), 0, 0, 0)
     out = new_languages_over_time(start, end)
@@ -71,7 +71,7 @@ Returns transcriber stats for "New transcripts" sheet
 TODO: Add year param //added 2/20/21
 """
 @csrf_exempt
-def transcriber_stats(request, start_year, start_month, start_day, end_year, end_month, end_day):
+def transcriber_stats(request, start_month, start_day, start_year, end_month, end_day, end_year):
     out = {}
     start = datetime(int(start_year), int(start_month), int(start_day), 0, 0, 0)
     end = datetime(int(end_year), int(end_month), int(end_day), 0, 0, 0)
